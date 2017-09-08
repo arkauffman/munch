9e43cbe33d78cdff0ac1045e3230932f16f4072c
Alexandra Kauffman <arkauffma@gmail.com>
2017-09-07 22:54:05 -0700

    updated styling of home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,13 +7,12 @@ const NavBar = (props) => {
     let nav = props.user ? 
         <div>
             <nav>
-                <Link to='/' className='Left'><span role="img" aria-label="Bread">🥖</span></Link>
+                <Link to='/' className='Left'><span role="img" aria-label="Bread" className="loaf">🥖</span>&nbsp;&nbsp; Welcome, {props.user.name}!</Link>
                 <Link to='/' className='brand-logo center'>Munchin'</Link>
-                <Link to='/logout' className='Right' onClick={props.handleLogout}><Icon tiny>exit_to_app</Icon></Link>
-                <Link to='/settings' className='Right'><Icon tiny>settings</Icon></Link>
-                <Link to='/search' className='Right'><Icon>search</Icon></Link>
+                <Link to='/logout' className='Right' onClick={props.handleLogout}><Icon tiny className="icon">exit_to_app</Icon></Link>
+                <Link to='/settings' className='Right'><Icon tiny className="icon">settings</Icon></Link>
+                <Link to='/search' className='Right'><Icon className="icon">search</Icon></Link>
             </nav>
-            <p className="Welcome">Welcome, {props.user.name}! </p>
         </div>
     :
         <div>
